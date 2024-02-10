c68157067afcbd4351e6b84b8ca9ee52ad850dfd
Irwaresly <silviairware372@gmail.com>
2024-02-10 01:25:06 -0800

    12

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 # script that sets up web servers for the deployment of web_static
+
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
+echo "<html>
   <head>
   </head>
   <body>
@@ -18,10 +15,15 @@ sudo echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/web_static
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+# Check if the location block already exists
+if ! grep -q "location /hbnb_static" /etc/nginx/sites-enabled/default; then
+    # Append the location block
+    sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+fi
 
+# Restart Nginx
 sudo service nginx restart
